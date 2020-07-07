f2778bf962bcf515dc254d838e79e632d6f0d264
unknown <f20170502@goa.bits-pilani.ac.in>
2020-07-07 12:20:32 +0530

    Updates

---
+++ precache-manifest.8945917967d4bb3930534f648cd627ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf312b32d2c13b49f88d1bb07348f1e",
+    "url": "/badmintonProject/index.html"
+  },
+  {
+    "revision": "0833b261b7b9a22576cb",
+    "url": "/badmintonProject/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "48c1055cbf222e4c2c06",
+    "url": "/badmintonProject/static/css/main.a52aae2e.chunk.css"
+  },
+  {
+    "revision": "0833b261b7b9a22576cb",
+    "url": "/badmintonProject/static/js/2.08565535.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/badmintonProject/static/js/2.08565535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c1055cbf222e4c2c06",
+    "url": "/badmintonProject/static/js/main.6aa351cd.chunk.js"
+  },
+  {
+    "revision": "099f739b9cc060b7c548",
+    "url": "/badmintonProject/static/js/runtime-main.9b5e6128.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/badmintonProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
